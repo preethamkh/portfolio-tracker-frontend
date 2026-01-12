5ce1beba5bf567f3fb94b2c8a501e664fb34c007
Preetham K H <blighter.soul@gmail.com>
2026-01-12 19:42:34 +1100

    clean up project as running into tsc and vite issues

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// PostCSS processes your CSS (Tailwind is a PostCSS plugin)
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}, // Adds vendor prefixes automatically (-webkit-, -moz-, etc.)
-  },
-};
