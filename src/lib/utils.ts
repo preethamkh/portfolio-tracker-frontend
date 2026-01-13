4b9f87f803bb9c973df35d7a95794da1ec372b41
Preetham K H <blighter.soul@gmail.com>
2026-01-13 14:19:50 +1100

    Update utility function for combining tailiwnd css classes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+/**
+ * Utility function for combining Tailwind CSS classes
+ * shadcn/ui uses this for conditional styling
+ *
+ * Example:
+ * cn("text-base", isActive && "font-bold", "text-red-500")
+ * → "text-base font-bold text-red-500" (if isActive is true)
+ *
+ * clsx:
+ * A library that takes any number of arguments (strings, arrays, objects)
+ *  and combines them into a single string of class names.
+ * 
+ * twMerge:
+ * A library that merges Tailwind CSS classes, removing duplicates
+ *  and resolving conflicts.
+
+ */
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
